2e481ff83f32c5a1ca1ce4a950e8224e4d433c61
Anshul Khanngwal <akhanngwal@gmail.com>
2026-09-07 16:01:32 +0530

    initiated authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,21 @@
 // import PrismaClient from '@prisma/client';
 // const prisma = new PrismaClient();
-
+// const bcrypt = require('bcrypt');
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+// const jwt = require('jsonwebtoken');
 import { db } from "../prisma/db.js";
 
 export async function handleRegistration(req, res){
     const {email, password, name, role} = req.body;
+    const hashedpass = await bcrypt.hash(password, 12);
     try {
         const newProvider = await db.orm.public.User.create({
         // data: {
             email: email,
-            password: password, // Make sure to hash this using bcrypt later!
+            password: hashedpass, // Make sure to hash this using bcrypt later!
             name: name,
-            role: 'SERVICE_PROVIDER' // Explicitly setting the role
+            role: role ?? 'SERVICE_PROVIDER' // Explicitly setting the role
         // },
         });
         console.log('Provider created:', newProvider);
@@ -23,6 +27,12 @@ export async function handleRegistration(req, res){
 
 export async function createAdmin(req, res){
     try {
+        const admin = await db.orm.public.User.where({role: "ADMIN"}).first()
+        if(admin){
+            res.status(201).json({
+                "message": "Already created."
+            })
+        }
         const newProvider = await db.orm.public.User.create({
         // data: {
             email: 'akhanngwal@gmail.com',
@@ -37,12 +47,46 @@ export async function createAdmin(req, res){
     }
 }
 
+export async function login(req, res){
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+            res.status(400).json({
+                "message": "Email and password is requied."
+            })
+        }
+        const user = db.orm.public.User.where(
+            {
+                email: email
+            }
+        ).first()
+        const match = bcrypt.compare(password, user.password);
+        if(!user){
+            res.status(404).json({
+                "message": "Not Found!"
+            })
+        }
+        if(!match){
+            res.status(400).json({
+                "message": "Wrong username or password!"
+            })
+        }
+        const accessToken = jwt.sign(JSON.stringify(user), process.env.TOKEN_SECRET);
+        res.status(200).json({
+            "accessToken": accessToken
+        })
+    }catch(e){
+        console.log(e)
+    }
+
+}
+
 export async function getUsers(req, res){
-    const plan = await db.orm.public.User.all()
-    console.log("Users", plan)
+    const users = await db.orm.public.User.all()
+    console.log("Users", users)
     res.status(200).json({
         "msg": "Success",
-        "data": plan
+        "data": users
     })
 
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 // const { PrismaClient } = require('@prisma/client');
 import express from 'express';
-import {handleRegistration, createAdmin, getUsers} from '../controllers/authController.js'
+import {handleRegistration, createAdmin, getUsers, login} from '../controllers/authController.js'
+import authenticateToken from '../middleware/authentication.js';
 
 const authRouter = express.Router()
 
@@ -8,7 +9,7 @@ const authRouter = express.Router()
 // Example of creating a service provider
 authRouter.post('/register', handleRegistration);
 authRouter.get('/createAdmin', createAdmin);
-authRouter.get('/getUsers', getUsers);
+authRouter.get('/getUsers', authenticateToken, getUsers);
+authRouter.get('/login', login);
 
 export default authRouter;
-
